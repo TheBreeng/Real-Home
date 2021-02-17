be8c76b970f42c7bb5db8d7e88ad99198c7aeb6b
Breeng <milniy.yasheg@gmail.com>
2021-02-17 16:02:01 +0300

    Пути...

--- src/js/modules/cards.js
+++ src/js/modules/cards.js
@@ -48,7 +48,7 @@ function cards(parentSelector, cardsStyle) {
         }
     }
 
-    getResource('https://thebreeng.github.io/Real-Home/db.json/cards')
+    getResource('db.json/cards')
         .then(data => {
             data.forEach(({
                 src,
