5099c762b47c41e0eaf86452c18566d65ae78b7d
Breeng <milniy.yasheg@gmail.com>
2021-02-24 23:03:17 +0300

    Порезал стили на компоненты

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,20 @@ window.addEventListener('DOMContentLoaded', function () {
 
     slider('.testimonials__slide', '.testimonials__wrapper', '.testimonials__inner', '.testimonials__indicators', 
     '.testimonials__next', '.testimonials__prev');
-    
+
     cards('.cards__wrapper', 'card');
+});
+
+
+const burger = document.querySelector('.burger-label');
+burger.addEventListener('click', () => {
+    let display = document.querySelector('.menu-mobile').style.display;
+    console.log(display);
+    if (display == 'none') {
+        document.querySelector('.menu-mobile').style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.querySelector('.menu-mobile').style.display = 'none';
+        document.body.style.overflow = '';
+    }
 });
